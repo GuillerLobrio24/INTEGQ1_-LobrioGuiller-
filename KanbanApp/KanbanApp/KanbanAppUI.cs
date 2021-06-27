281f53d68891d86e045536d3168fbdd12d2e2193
GuillerLobrio24 <82878247+GuillerLobrio24@users.noreply.github.com>
2021-06-27 09:43:12 -0700

    Add files via upload

---
+++ KanbanApp/KanbanApp/KanbanAppUI.cs
@@ -0,0 +1,34 @@
+﻿using KanbanAppBL;
+using System;
+
+namespace KanbanApp
+{
+    class KanbanAppUI
+    {
+        static void Main(string[] args)
+        {
+            AdminAccount user = new AdminAccount();
+            
+
+            Console.WriteLine("Welcome to Kanban Project Management App");
+            Console.WriteLine("Enter Admin Account (1)");
+            Console.WriteLine("Signup (2)");
+            
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput.Equals(1))
+            {
+                user.LogIn();
+            }
+            else if (userInput.Equals(2))
+            {
+                user.Signup();
+            }
+            else
+            {
+
+            }
+            
+        }
+    }
+} 
