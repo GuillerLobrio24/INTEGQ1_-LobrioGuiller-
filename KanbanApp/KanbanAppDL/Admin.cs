bcb4dd8f249f8800d2fc37dbbdaf5d6241430622
GuillerLobrio24 <82878247+GuillerLobrio24@users.noreply.github.com>
2021-06-27 09:57:14 -0700

    Add files via upload

---
+++ KanbanApp/KanbanAppDL/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace KanbanAppDL
+{
+    public class Admin
+    {
+        private string adminuser;
+        private string adminpass;
+        public Admin(string adminuser, string adminpass)
+        {
+            this.adminuser = adminuser;
+            this.adminpass = adminpass;
+        }
+
+        public string UserName
+        {
+            get { return adminuser; }
+            set { adminuser = value; }
+        }
+        public string PassWord
+        {
+            get { return adminpass; }
+            set { adminpass = value; }
+        }
+    }
+}
---
+++ KanbanApp/KanbanAppDL/Category.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KanbanAppDL
+{
+    public class Category
+    {
+        public string unopened;
+        public string opened;
+        public string finished;
+        public string late;
+
+        public Category(string unopened, string opened, string finished, string late)
+        {
+            this.unopened = unopened;
+            this.opened = opened;
+            this.finished = finished;
+            this.late = late;
+        }
+        public string Unopened
+        {
+            get { return unopened; }
+            set { unopened = value; }
+        }
+
+        public string Opened
+        {
+            get { return opened; }
+            set { opened = value; }
+        }
+
+        public string Finished
+        {
+            get { return finished; }
+            set { finished = value; }
+        }
+
+        public string Late
+        {
+            get { return late; }
+            set { late = value; }
+        }
+    }
+}
